07bcc94a8fa80ab5f5794c838869deaf4cbea520
Nixie-Para-unda <paraundanixie82@gmail.com>
2025-03-25 10:30:31 +0800

    not hashed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'password' =>$request->password,
         ]);
 
         return response()->json(['message' => 'User registered successfully'], 201);
